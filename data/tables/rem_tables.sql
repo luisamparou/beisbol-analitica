85c561f7d72055902d8a9c5515cac6f4a593fdbf
Juanito Banca <jose_alvarado89@hotmail.com>
2020-12-12 17:38:26 -0800

    INT(11) por INTEGER.
    Cambiando las palabras reservadas.

--- data/tables/rem_tables.sql
+++ data/tables/rem_tables.sql
@@ -3,24 +3,24 @@ USE baseball;
 DROP TABLE rem_play_by_play;
 
 CREATE TABLE IF NOT EXISTS rem_play_by_play (
-  majorLeagueId int(11),
+  majorLeagueId INTEGER,
   seasonId VARCHAR(20),
-  venueId int(11),
-  gamePk int(11),
-  inning int(11),
+  venueId INTEGER,
+  gamePk INTEGER,
+  inning INTEGER,
   halfInning VARCHAR(15),
-  atBatIndex int(11),
-  playIndex int(11),
+  atBatIndex INTEGER,
+  playIndex INTEGER,
   event VARCHAR(100),
   runnersBeforePlay VARCHAR(3),
-  runsScoredBeforePlay int(11),
-  outsBeforePlay int(11),
-  runsScoredInPlay int(11),
-  outsInPlay int(11),
-  runsScoredAfterPlay int(11),
-  outsAfterPlay int(11),
+  runsScoredBeforePlay INTEGER,
+  outsBeforePlay INTEGER,
+  runsScoredInPlay INTEGER,
+  outsInPlay INTEGER,
+  runsScoredAfterPlay INTEGER,
+  outsAfterPlay INTEGER,
   runnersAfterPlay VARCHAR(3),
-  runsScoredEndInning int(11)
+  runsScoredEndInning INTEGER
 );
 
 ALTER TABLE rem_play_by_play ADD INDEX(gamePk, atBatIndex, playIndex);
